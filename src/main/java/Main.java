ef2173e113b4c857cede599771d3eebfbfa414cb
maxzenin <max.zenin@gmail.com>
2019-05-16 18:58:08 +1000

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,12 +8,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
 
-        models.Users users = new Users();
-        models.Records records = new Records();
-        System.out.println("Hello world");
 
-
-
-        System.out.println(users.getId());
     }
 }
